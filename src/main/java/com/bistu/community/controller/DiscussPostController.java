a4c616c1b7be01a8a4fde014ad9452de879054d9
雨歌 <guoyvnan@126.com>
2023-05-22 15:45:22 +0800

    显示评论功能开发完毕

--- src/main/java/com/bistu/community/controller/DiscussPostController.java
+++ src/main/java/com/bistu/community/controller/DiscussPostController.java
@@ -1,9 +1,13 @@
 package com.bistu.community.controller;
 
+import com.bistu.community.entity.Comment;
 import com.bistu.community.entity.DiscussPost;
+import com.bistu.community.entity.Page;
 import com.bistu.community.entity.User;
+import com.bistu.community.service.CommentService;
 import com.bistu.community.service.DiscussPostService;
 import com.bistu.community.service.UserService;
+import com.bistu.community.util.CommunityConstant;
 import com.bistu.community.util.CommunityUtil;
 import com.bistu.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostController {
+public class DiscussPostController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
@@ -28,6 +32,9 @@ public class DiscussPostController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
 
     // 处理请求
     @RequestMapping(path = "/add", method = RequestMethod.POST)
@@ -53,14 +60,62 @@ public class DiscussPostController {
     // 像这种只需要id的方法，通常会在path中添加上id
     // 因为这个方法需要返回模板 所以不加@ResponseBody注解
     // @PathVariable可以将方法内的参数提交到上面的path中
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page) {
         // 查询帖子
         DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
         model.addAttribute("post", post);
         // 作者
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user", user);
-        // 回复（待开发）
+        // 评论的分页信息
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        // 此处使用的字段是DisscussPost表里面的，之前为了方便在这个表中对评论的数量做了一个冗余存储
+        page.setRows(post.getCommentCount());
+        // 这里的实体类型最好做成可以改动的，这里的做法是将实体类型存储在常量中
+        // 评论：帖子的评论
+        // 回复：评论的评论
+        // 评论列表
+        List<Comment> commentList =  commentService.findCommentByEntity(ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimit());
+        // 评论VO列表
+        List<Map<String , Object>> commentVoList = new ArrayList<>();
+        if (commentList != null) {
+            for (Comment comment : commentList) {
+                Map<String, Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment", comment);
+                // 评论的作者
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+
+                // 回复列表
+                List<Comment> replyList = commentService.findCommentByEntity(ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE);
+                // 回复VO列表
+                List<Map<String , Object>> replyVoList = new ArrayList<>();
+                if (replyList != null) {
+                    for (Comment reply : replyList) {
+                        Map<String, Object> replyVo = new HashMap<>();
+                        // 回复
+                        replyVo.put("reply", reply);
+                        // 作者
+                        replyVo.put("user", userService.findUserById(reply.getUserId()));
+                        // 回复目标
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getUserId());
+                        replyVo.put("target", target);
+
+                        replyVoList.add(replyVo);
+                    }
+                }
+                commentVo.put("relays" ,replyVoList);
+                // 回复数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+
+                commentVoList.add(commentVo);
+            }
+        }
+
+        model.addAttribute("comments", commentVoList);
+
         return "/site/discuss-detail";
     }
 
---
+++ src/main/java/com/bistu/community/dao/CommentMapper.java
@@ -0,0 +1,15 @@
+package com.bistu.community.dao;
+
+import com.bistu.community.entity.Comment;
+import com.bistu.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    // 因为在页面上要对评论进行分页处理,同时也要判断评论的实体类型，下面这个方法的返回值是list，目的是规定每一页上有多少条评论，这样方便分页
+    List<Comment> selectCommentByEntity(int entityType, int entityId, int offset, int limit);
+    // 返回评论的数量
+    int selectCountByEntity(int entityType, int entityId);
+}
