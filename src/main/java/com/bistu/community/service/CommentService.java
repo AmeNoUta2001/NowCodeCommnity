77704220a6b52cce677a5f3e4848cedefce9d11d
雨歌 <guoyvnan@126.com>
2023-05-24 09:08:09 +0800

    添加评论开发完毕+私信列表实体类和Mapper开发中

--- src/main/java/com/bistu/community/service/CommentService.java
+++ src/main/java/com/bistu/community/service/CommentService.java
@@ -2,23 +2,55 @@ package com.bistu.community.service;
 
 import com.bistu.community.dao.CommentMapper;
 import com.bistu.community.entity.Comment;
+import com.bistu.community.util.CommunityConstant;
+import com.bistu.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
     @Autowired
     private CommentMapper commentMapper;
 
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
-        return commentMapper.selectCommentByEntity(entityType,entityId,offset,limit);
+        return commentMapper.selectCommentsByEntity(entityType,entityId,offset,limit);
     }
 
     public int findCommentCount(int entityType, int entityId) {
         return commentMapper.selectCountByEntity(entityType, entityId);
     }
 
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+        if (comment == null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        // 添加评论，并对评论中的内容进行过滤
+        // 传入过滤之前的内容
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        // 过滤敏感词
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // 更新帖子的评论数量
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(),comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), count);
+        }
+
+        return rows;
+    }
 
 }
